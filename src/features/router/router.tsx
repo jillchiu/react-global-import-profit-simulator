4d9cd7ceebffbc07ae0969326f12f1b714438232
jillchiu <kk923049@gmail.com>
2026-07-08 22:19:53 +0900

    Fix asset base path for GitHub Pages deployment

--- src/features/router/router.tsx
+++ src/features/router/router.tsx
@@ -1,15 +1,20 @@
-import { createBrowserRouter } from "react-router-dom"
-import SimulatorPage from "../pages/SimulatorPage"
-import ProductPage from "../pages/ProductPage"
-import SettingsPage from "../pages/SettingsPage"
-import RootLayout from "../pages/RootLayout"
+import { createBrowserRouter } from "react-router-dom";
+import SimulatorPage from "../pages/SimulatorPage";
+import ProductPage from "../pages/ProductPage";
+import SettingsPage from "../pages/SettingsPage";
+import RootLayout from "../pages/RootLayout";
 
-export const router = createBrowserRouter([
-    { path: "/", element: <RootLayout />, children: [
-        { index: true, element: <SimulatorPage />},
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        { index: true, element: <SimulatorPage /> },
         { path: "/products", element: <ProductPage /> },
-        { path: "/settings", element: <SettingsPage /> }
-        
-    ]}
-
-])
+        { path: "/settings", element: <SettingsPage /> },
+      ],
+    },
+  ],
+  { basename: "/react-global-import-profit-simulator" },
+);
